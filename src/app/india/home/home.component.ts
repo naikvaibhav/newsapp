796fb97cc232db46b9ea3cedaab3f219be251cf7
naikvaibhav <naikvaibhav1994@gmail.com>
2020-07-29 12:03:16 +0530

    upgraded to angular 8

--- src/app/india/home/home.component.ts
+++ src/app/india/home/home.component.ts
@@ -1,36 +1,34 @@
 /*this home component calls api to fetch headlines from india*/
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 /*importing services*/
-import { HeadlinesService } from './../../services/headlines.service';
+import { HeadlinesService } from "./../../services/headlines.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
   public headlines;
 
   constructor(public headlinesHttpService: HeadlinesService) {
-    console.log('Home component constructor is called');
+    console.log("Home component constructor is called");
   }
 
   ngOnInit() {
-    console.log('Home component onInit called');
+    console.log("Home component onInit called");
     /*subscribing to observables*/
     this.headlinesHttpService.getHeadlinesOfIndia().subscribe(
-      data => {
-        console.log('logging data');
+      (data) => {
+        console.log("logging data");
         this.headlines = data["articles"];
         console.log(this.headlines);
       },
       /*handle error if not subscribed*/
-      error => {
+      (error) => {
         console.log("some error occured");
-        console.log(error.errorMessage);
+        console.log(error);
       }
-    )
-
+    );
   }
-
 }
